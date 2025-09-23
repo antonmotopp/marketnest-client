ef60e9357075bf98e62fbe4b8a52bba510387c0d
Anton <anton.merkotun@motopp.nl>
2025-09-23 13:37:18 +0200

    fix(axios): update api link

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores';
 
 export const api = axios.create({
-  baseURL: import.meta.env.API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
